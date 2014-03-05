3a4529e2436c13036764bfc98be6a4f1549576ed
Evgeny Blinov <e.a.blinov@gmail.com>
2014-03-06 01:09:28 +0300

    OOP restyling

--- translit.php
+++ translit.php
@@ -12,58 +12,64 @@
  * в остальных случаях - "cz"
  * @return string
  */
+class Translit {
 
-define("TRANSLIT_I_VARIANT2", 1);
-define("TRANSLIT_C_ONLY", 2);
-define("TRANSLIT_CZ_ONLY", 4);
+    const TRANSLIT_I_VARIANT2 = 1;
+    const TRANSLIT_C_ONLY     = 2;
+    const TRANSLIT_CZ_ONLY    = 4;
 
-function translit($string, $params = 0) {
+    public static function transliterate($string, $params = 0) {
 
-    // обычные буквы (односимвольные транслитерации)
+        // обычные буквы (односимвольные транслитерации)
 
-    $cyr = 'абвгдезйклмнопрстуфхьАБВГДЕЗЙКЛМНОПРСТУФХЬ';
-    $lat = 'abvgdezjklmnoprstufx`ABVGDEZJKLMNOPRSTUFX`';
+        $cyr = 'абвгдезйклмнопрстуфхьАБВГДЕЗЙКЛМНОПРСТУФХЬ';
+        $lat = 'abvgdezjklmnoprstufx`ABVGDEZJKLMNOPRSTUFX`';
 
-    $result = strtr($string, $cyr, $lat);
+        $result = strtr($string, $cyr, $lat);
 
-    // многобуквенные транслитерации (кроме Ц)
+        // многобуквенные транслитерации (кроме Ц)
 
-    $cyr2lat = array(
-        'ё'=> 'yo',  'Ё'=> 'Yo',
-        'ж'=> 'zh',  'Ж'=> 'Zh',
-        'ч'=> 'ch',  'Ч'=> 'Ch',
-        'ш'=> 'sh',  'Ш'=> 'Sh',
-        'щ'=> 'shh', 'Щ'=> 'Shh',
-        'ъ'=> '``',  'Ъ'=> '``',
-        'ы'=> 'y\'', 'Ы'=> 'Y\'',
-        'э'=> 'e`',  'Э'=> 'E`',
-        'ю'=> 'yu',  'Ю'=> 'Yu',
-        'я'=> 'ya',  'Я'=> 'Ya',
-        'и'=> $params & TRANSLIT_I_VARIANT2?'i\'':'i',
-        'И'=> $params & TRANSLIT_I_VARIANT2?'И\'':'И',
-    );
+        $cyr2lat = array(
+            'ё' => 'yo', 'Ё' => 'Yo',
+            'ж' => 'zh', 'Ж' => 'Zh',
+            'ч' => 'ch', 'Ч' => 'Ch',
+            'ш' => 'sh', 'Ш' => 'Sh',
+            'щ' => 'shh', 'Щ' => 'Shh',
+            'ъ' => '``', 'Ъ' => '``',
+            'ы' => 'y\'', 'Ы' => 'Y\'',
+            'э' => 'e`', 'Э' => 'E`',
+            'ю' => 'yu', 'Ю' => 'Yu',
+            'я' => 'ya', 'Я' => 'Ya',
+            'и' => $params & self::TRANSLIT_I_VARIANT2 ? 'i\'' : 'i',
+            'И' => $params & self::TRANSLIT_I_VARIANT2 ? 'И\'' : 'И',
+        );
 
-    $result = strtr($result, $cyr2lat);
+        $result = strtr($result, $cyr2lat);
 
-    // поведение для Ц
+        // поведение для Ц
 
-    if(($params & TRANSLIT_C_ONLY) && ($params & TRANSLIT_CZ_ONLY)) {
-        user_error('Using both TRANSLIT_C_ONLY and TRANSLIT_CZ_ONLY params. Using rule TRANSLIT_C_ONLY.', E_NOTICE);
-        $result = strtr($result, 'цЦ','cC');
-    } elseif($params & TRANSLIT_C_ONLY) {
-        $result = strtr($result, 'цЦ','cC');
-    } elseif($params & TRANSLIT_CZ_ONLY) {
-        $result = strtr($result, array('ц' => 'cz', 'Ц' => 'Cz', ));
-    } else {
-        // используем рекомендованые правила
-        $result = strtr($result, array(
-            'цi' => 'ci', 'Цi' => 'Ci',
-            'цe' => 'ce', 'Цe' => 'Ce',
-            'цy' => 'cy', 'Цy' => 'Cy',
-            'цj' => 'cj', 'Цj' => 'Cj',
-            'ц'  => 'cz', 'Ц'  => 'Cz'
-        ));
+        if (($params & self::TRANSLIT_C_ONLY) && ($params & self::TRANSLIT_CZ_ONLY)) {
+            trigger_error('Using both TRANSLIT_C_ONLY and TRANSLIT_CZ_ONLY params. Using rule TRANSLIT_C_ONLY.', E_NOTICE);
+            $result = strtr($result, 'цЦ', 'cC');
+        }
+        elseif ($params & self::TRANSLIT_C_ONLY) {
+            $result = strtr($result, 'цЦ', 'cC');
+        }
+        elseif ($params & self::TRANSLIT_CZ_ONLY) {
+            $result = strtr($result, array('ц' => 'cz', 'Ц' => 'Cz',));
+        }
+        else {
+            // используем рекомендованые правила
+            $result = strtr($result, array(
+                'цi' => 'ci', 'Цi' => 'Ci',
+                'цe' => 'ce', 'Цe' => 'Ce',
+                'цy' => 'cy', 'Цy' => 'Cy',
+                'цj' => 'cj', 'Цj' => 'Cj',
+                'ц'  => 'cz', 'Ц'  => 'Cz'
+            ));
+        }
+
+        return $result;
     }
 
-    return $result;
 }
